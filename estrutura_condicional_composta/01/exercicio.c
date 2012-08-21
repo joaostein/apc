4984680d9495acc42251f6c825c1b510f200291f
Joao Stein <joaosteing@gmail.com>
2012-08-21 12:27:47 -0300

    Finalizado exercicio 01 de estruturas condicionais compostas

--- estrutura_condicional_composta/01/exercicio.c
+++ estrutura_condicional_composta/01/exercicio.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#define PI 3.14159
 
 int main() {
 
+    int raio; 
+    float area;
+
+    printf("Informe o valor do raio: ");
+    scanf("%d", &raio);
+
+    if ( raio > 0 ) {
+      area = PI * raio * raio;
+      printf("\nArea = %f\n\n", area);
+    } else {
+      printf("Raio nao pode ser negativo\n\n");
+    }
 
     system("pause");
 }

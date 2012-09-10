d83d7bfcd8da95e649dd7d38d4a8aa9b38bef5b6
Joao Stein <joaosteing@gmail.com>
2012-09-10 11:45:17 -0300

    Finalizado exercicio 01

--- estrutura_condicional/01/exercicio.c
+++ estrutura_condicional/01/exercicio.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  
+  int base, altura, area;
+
+  printf("Informe a base do triangulo: ");
+  scanf("%d", &base);
+
+  printf("Informe a altura do triangulo: ");
+  scanf("%d", &altura);
+
+  if (base <= 0 || altura <=0 ) {
+    printf("Dados fornecidos nao podem ser negativos ou igual a zero\n");
+  } else {
+    area = ( base * altura ) / 2;
+    printf("A area do triangulo = %d\n\n", area);
+  }
+
 
   system("pause");
 }

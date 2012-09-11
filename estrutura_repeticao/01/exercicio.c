77bd1f239cb554e1c6433adda4823e765475acbb
Joao Stein <joaosteing@gmail.com>
2012-09-11 11:01:14 -0300

    Finalizado exercicio 01 da lista de estrutura de repeticao

--- estrutura_repeticao/01/exercicio.c
+++ estrutura_repeticao/01/exercicio.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    
+    int n, soma = 0, i = 1;
+
+    printf("Informe o valor de N: ");
+    scanf("%d", &n);
+
+    for ( i; i <= n; i++) {
+        soma += i;
+    }
+
+    printf("soma: %d\n", soma);
+
     system("pause");
 }

db69a66be1df76b87f59b2f4a5833ac20b13f08e
Joao Stein <joaosteing@gmail.com>
2012-11-20 14:40:38 -0200

    Finalizado exercício 03 de strings

--- strings/pdf/03/exercicio.c
+++ strings/pdf/03/exercicio.c
@@ -3,5 +3,22 @@
 #include <string.h>
 
 int main() {
+  char string[50], caractere[3], inteiro[20];
+  int posicao;
+
+  printf("Informe uma string: ");
+  fgets(string, 50, stdin);
+
+  printf("Informe um caractere: ");
+  fgets(caractere, 3, stdin);
+
+  do {
+    printf("Informe um inteiro de 1 a %zu, que representa a posiçao da letra: ", strlen(string) - 1);
+    fgets(inteiro, 20, stdin);
+    posicao = atoi(inteiro) - 1;
+  } while (posicao >= strlen(string) - 2 || posicao <= 0);
+
+  string[posicao] = caractere[0];
+  puts(string);
 
 }

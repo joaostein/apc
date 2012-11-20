0f852f30d1173e8dbd02ba9d398540d5c8bb95eb
Joao Stein <joaosteing@gmail.com>
2012-11-20 15:15:51 -0200

    Finalizado exercício 04 de strings

--- strings/pdf/04/exercicio.c
+++ strings/pdf/04/exercicio.c
@@ -4,4 +4,24 @@
 
 int main() {
 
+  char string[50], caractere_1[3], caractere_2[3];
+  int i;
+
+  printf("Informe um texto: ");
+  fgets(string, 50, stdin);
+
+  printf("Informe um caractere: ");
+  fgets(caractere_1, 3, stdin);
+
+  printf("Informe outro caractere: ");
+  fgets(caractere_2, 3, stdin);
+
+  for (i = 0; i <= strlen(string) - 1; i++) {
+    if (string[i] == caractere_1[0]) {
+      string[i] = caractere_2[0];
+    }
+  }
+
+  puts(string);
+
 }

5f2e1f92ee3b3894a7ba21f9ae8a55a7db2b189e
Joao Stein <joaosteing@gmail.com>
2012-11-20 14:06:32 -0200

    Finalizado exercício 02 de strings

--- strings/pdf/02/exercicio.c
+++ strings/pdf/02/exercicio.c
@@ -4,5 +4,16 @@
 
 int main() {
 
+  char string[20], string_inversa[20];
+  int i, aux = 0;
 
+  printf("Informe uma string: ");
+  fgets(string, 20, stdin);
+
+  for (i = strlen(string) - 1; i >= 0; i--) {
+    string_inversa[aux] = string[i];
+    aux++;
+  }
+  
+  printf("%s \n", string_inversa);
 }
